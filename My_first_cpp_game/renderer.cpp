89b80dd069c5fa9f18ae41f922c463e8a90e0f48
MarinEstrada <adrian.marin.estrada@gmail.com>
2026-03-06 21:58:14 -0800

    DOCS: comment out unused func

--- My_first_cpp_game/renderer.cpp
+++ My_first_cpp_game/renderer.cpp
@@ -1,14 +1,15 @@
-void render_background() {
-	unsigned int* pixel = (unsigned int*)render_state.memory; //ptr to current pixel
-	for (int y = 0; y < render_state.height; y++) {
-		for (int x = 0; x < render_state.width; x++) {
-			//*pixel++ = 0xff5500; //this line assigned *pixel to 0xff5500, then afterwards it increments the ptr pixel by one
-			//*pixel++ = 0xff5500 * x + 0xff5500 * y;
-			//*pixel++ = (x + y) * 2;
-			*pixel++ = 0xff00ff * x + 0x00ff00 * y;
-		}
-	}
-}
+//void render_background() {
+//	uint32* pixel = (uint32*)render_state.memory; //ptr to current pixel
+//	for (int y = 0; y < render_state.height; y++) {
+//		for (int x = 0; x < render_state.width; x++) {
+//			//*pixel++ = 0xff5500; //this line assigned *pixel to 0xff5500, then afterwards it increments the ptr pixel by one
+//			//*pixel++ = 0xff5500 * x + 0xff5500 * y;
+//			//*pixel++ = (x + y) * 2;
+//			*pixel++ = 0xff00ff * x + 0x00ff00 * y;
+//		}
+//	}
+//}
+
 
 void clear_screen(unsigned int color) {
 	unsigned int* pixel = (unsigned int*)render_state.memory;
