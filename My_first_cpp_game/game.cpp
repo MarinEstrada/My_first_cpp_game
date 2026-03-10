5707bd767eafc0a235ea1b70cd36bcf8ea9a2fd8
MarinEstrada <adrian.marin.estrada@gmail.com>
2026-03-09 17:26:21 -0700

    DOCS: correction on theory in comments
    Have units per second and want units per frame

--- My_first_cpp_game/game.cpp
+++ My_first_cpp_game/game.cpp
@@ -5,15 +5,15 @@
 //float player_pos = 0.f;
 float x_player_pos = 0.f;
 float y_player_pos = 0.f;
-float a_movement = 0.1f; //currently this number is units per frame... need to make it units per second
+float movement_speed = 0.1f; //currently this number is units per second... need to make it units per frame
 
 static void simulate_game(Input* input) {
 		//render_background();
 		clear_screen(0x00009f);
-		if (pressed(BUTTON_UP)) x_player_pos += a_movement;
-		if (pressed(BUTTON_DOWN)) x_player_pos -= a_movement;
-		if (pressed(BUTTON_RIGHT)) y_player_pos += a_movement;
-		if (pressed(BUTTON_LEFT)) y_player_pos -= a_movement;
+		if (pressed(BUTTON_UP)) x_player_pos += movement_speed;
+		if (pressed(BUTTON_DOWN)) x_player_pos -= movement_speed;
+		if (pressed(BUTTON_RIGHT)) y_player_pos += movement_speed;
+		if (pressed(BUTTON_LEFT)) y_player_pos -= movement_speed;
 		draw_rect(y_player_pos, x_player_pos, 1, 1, 0xff5522);
 		//if (input->buttons[BUTTON_RIGHT].is_down)
 		//	draw_rect(50, 20, 25, 3, 0x0088fe);
